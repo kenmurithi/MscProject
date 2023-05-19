4e2b25ac1c17c5fa18629664db7c286ac1c9a398
Ken Murithi <59683723+kenmurithi@users.noreply.github.com>
2023-05-19 17:46:18 -0400

    Bedtools script

---
+++ Scripts/bedtools.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+# Name: bedtools.sh
+# Author: Ken Murithi
+# Date: 5 Jan 2022
+
+#create a directory with bed files for each of the Glossina species
+#For example: nano Gau-or # this directory contins bed files for the glossina austeni chemosensory genes
+
+for gff in  *.bed
+
+do
+
+base=$(basename ${gff})
+base2=$(basename $base .bed)
+
+echo 'running' $base2
+
+bedtools flank -i $gff -g Gau.genome -l 500 -r 0 >  ${base2}_upstream.bed # extracts the upstream (500bp) region of the chemosensory genes
+bedtools getfasta -fi ./data/index_genomes/${genome} -bed ${base2}_upstream.bed >${base2}_upstream.fa #gets the fasta file of the extracted upstream region
+
+done
